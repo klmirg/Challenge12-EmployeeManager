148eb9ecd31a48fafb9fd5336bb62648b8a25ee7
Ronya <klmirg6@gmail.com>
2021-12-30 23:12:12 -0600

    Setting up the tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,22 @@
-INSERT INTO department
+  INSERT INTO employee (first_name, last_name, id)
+VALUES   
+  ('Ronya', 'Gettis', 1, )
+  ('Giang', 'Nguyen', 2, )
+  ('Paige', 'Kleeberger', 3, )
+  ('Ana', 'Block', 4, )
+  ('Sofia', 'Nguyen', 5, )
+  ('Rachel', 'Gettis', 6, )
+  ('Erin', 'Kleeberger', 7, )
+  ('Paul', 'Erickson', 8, )
+  ('Jon', 'Wertjes', 9, )
+
+  INSERT INTO role (job_title, salary, department_id)
+VALUES
+
+  INSERT INTO department (departmentName)
+VALUES
+  ('Engineering'), 
+  ('Finance'), 
+  ('Legal'),
+  ('Sales');
+
--- index.js
+++ index.js
@@ -1,15 +1,109 @@
-// const fs = require("fs")
-// const path = require("path")
+const fs = require("fs")
+const path = require("path")
 const inquirer = require("inquirer")
 
+const employees = [];
+
+function viewDepartments (){
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "whichDepartment",
+      message: "Which department would you like to see employees for?",
+      choices: ["Engineering", "Finance", "Legal", "Sales"]
+    }
+  ])
+}
+
+function viewRoles () {
+
+}
+
+function viewEmployees () {
+
+}
+
+function addDepartment (){
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "departmentName",
+      message: "What is the name of the department?"
+    },
+  ]).then(answers =>{
+    const department = new Department(answers.departmentName)
+    employees.push(department)
+    mainMenu()
+  })
+}
 
 function mainMenu(){
   inquirer.prompt([
     {
       type: "list",
-      name: "",
+      name: "memberChoice",
       message: "What would you like to do?",
-      choices: [""]
-    },
-  ]);
+      choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role" ]
+    }
+  ]).then(userChoice =>{
+    if (userChoice.memberChoice === "View All Departments") {
+      viewDepartments();
+    } else if (userChoice.memberChoice === "View All Roles") {
+      viewRoles();
+    } else if (userChoice.memberChoice === "View All Employees") {
+      viewEmployees();
+    } else if (userChoice.memberChoice === "Add Department") {
+      addDepartment();
+    } else if (userChoice.memberChoice === "Add Role") {
+      addRole();
+    } else if (userChoice.memberChoice === "Add Employee") {
+      addEmployee();
+    } else {
+      console.log(employees)
+      updateEmployeeRole();
+    }
+  })
 };
+
+function addRole(){
+  inquirer.prompt([
+    {
+
+    }
+  ])
+}
+
+function addEmployee(){
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "firstName",
+      message: "What is the employee's first name?"
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: "What is the employee's last name?"
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "What is the employee's role?",
+      choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"]
+    },
+    {
+      type: "list",
+      name: "employeeManager",
+      message: "Who is the employee's manager?",
+      choices: [""]
+    }
+  ])
+}
+
+// .then(function ({ first_name, last_name, manager }) {
+//   connection.query("INSERT INTO employee (first_name, last_name, manager) 
+//        VALUES ?", ('first_name', 'last_name', 'manager'), function (err, result) {
+//       if (err) throw err;
+// })
+
+mainMenu();
